bfb304d65cd86ed9364d9822accfb8d7ebeedd4d
Mr.MKZ <karimierfan74@gmail.com>
2024-05-29 15:40:55 +0330

    add icon for some detected files

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, redirect, send_from_directory
-import win32api
 import os
+
+import win32api
+from flask import Flask, render_template, redirect, send_from_directory
 
 
 def get_drives():
@@ -12,6 +13,34 @@ def get_drives():
 def cd(path):
     os.chdir(path)
     return os.getcwd()
+
+
+def file_icon(filename):
+    file_type = filename.split(".")[-1]
+    extensions = {
+        "txt": "text-icon",
+        "xlsx": "excel-icon",
+        "docx": "word-icon",
+        "doc": "word-icon",
+        "pdf": "pdf-icon",
+        "exe": "exe-icon",
+        "zip": "archive-icon",
+        "7z": "archive-icon",
+        "rar": "rar-icon",
+        "mp4": "video-icon",
+        "mkv": "video-icon",
+        "avi": "video-icon",
+        "flv": "video-icon",
+        "webm": "video-icon",
+        "wmv": "video-icon",
+        "png": "image-icon",
+        "jpg": "image-icon",
+        "jpeg": "image-icon",
+        "gif": "image-icon",
+        "bmp": "image-icon",
+        "raw": "image-icon"
+    }
+    return extensions.get(file_type.lower(), "unknown-icon")
 
 
 app = Flask(__name__)
@@ -26,7 +55,8 @@ def main_page():
             "name": i.name,
             "isDir": i.is_dir(),
             "isFile": i.is_file(),
-            "isSymlink": i.is_symlink()
+            "isSymlink": i.is_symlink(),
+            "icon": "folder-icon" if i.is_dir() else file_icon(i.name)
         }
         dir_contents.append(content_dic)
 
@@ -35,7 +65,8 @@ def main_page():
     else:
         dir_contents.insert(0, False)
 
-    return render_template("main_page.html", dir_contents=dir_contents, drives=get_drives(), current_drive=os.getcwd().split("\\")[0] + "\\", current_path=os.getcwd())
+    return render_template("main_page.html", dir_contents=dir_contents, drives=get_drives(),
+                           current_drive=os.getcwd().split("\\")[0] + "\\", current_path=os.getcwd())
 
 
 @app.route("/download/<path:filepath>")
@@ -45,7 +76,9 @@ def download(filepath):
     try:
         return send_from_directory(dir, filename)
     except PermissionError:
-        return render_template("error.html", error_title="Permission denied", error_description="Sorry but you don't have permission to download this file!")
+        return render_template("error.html", error_title="Permission denied",
+                               error_description="Sorry but you don't have permission to download this file!")
+
 
 @app.route("/prev_dir")
 def prev_dir():
@@ -59,13 +92,15 @@ def prev_dir():
             cd(prev_path[0] + "\\")
         return redirect("/")
 
+
 @app.route("/chdir/<path:dirpath>")
 def change_directory(dirpath):
     try:
         cd(dirpath)
         return redirect("/")
     except PermissionError:
-        return render_template("error.html", error_title="Permission denied", error_description="Sorry but you don't have permission to open this folder")
+        return render_template("error.html", error_title="Permission denied",
+                               error_description="Sorry but you don't have permission to open this folder")
 
 
 if __name__ == '__main__':
