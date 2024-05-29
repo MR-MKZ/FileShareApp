ef0b9b7d8d7191e12c6f60acfd107a4686004c09
Mr.MKZ <karimierfan74@gmail.com>
2024-05-29 16:31:00 +0330

    A series of changes to fix performance issues in Linux

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import os
 
-import win32api
+import psutil
+import platform
 from flask import Flask, render_template, redirect, send_from_directory
 
 
 def get_drives():
-    drives = win32api.GetLogicalDriveStrings()
-    drives = drives.split('\000')[:-1]
+    drives = []
+    for disk in psutil.disk_partitions():
+        drives.append(disk.mountpoint)
     return drives
 
 
 def cd(path):
-    os.chdir(path)
+    os.chdir(path.replace("/", "\\") if platform.system() == "Windows" else path)
     return os.getcwd()
 
 
@@ -60,13 +62,13 @@ def main_page():
         }
         dir_contents.append(content_dic)
 
-    if len(os.getcwd().split("\\")) > 1 and os.getcwd().split("\\")[1] != '':
+    if len(os.getcwd().replace("\\", "/").split("/")) > 1 and os.getcwd().replace("\\", "/").split("/")[1] != '':
         dir_contents.insert(0, True)
     else:
         dir_contents.insert(0, False)
 
     return render_template("main_page.html", dir_contents=dir_contents, drives=get_drives(),
-                           current_drive=os.getcwd().split("\\")[0] + "\\", current_path=os.getcwd())
+                           current_drive=os.getcwd().split("/")[0] + "\\" if platform.system() == "Windows" else "/" + os.getcwd().split("/")[1], current_path=os.getcwd())
 
 
 @app.route("/download/<path:filepath>")
@@ -82,14 +84,14 @@ def download(filepath):
 
 @app.route("/prev_dir")
 def prev_dir():
-    print(len(os.getcwd().split("\\")) > 1 and os.getcwd().split("\\")[1] != '')
-    if len(os.getcwd().split("\\")) > 1 and os.getcwd().split("\\")[1] != '':
-        prev_path = os.getcwd().split("\\")
+    prev_path = os.getcwd().replace("\\", "/").split("/")
+    print(len(prev_path) > 1 and prev_path[1] != '')
+    if len(prev_path) > 1 and prev_path[1] != '':
         del prev_path[-1]
         if len(prev_path) > 1:
-            cd("\\".join(prev_path))
+            cd("/".join(prev_path))
         else:
-            cd(prev_path[0] + "\\")
+            cd(prev_path[0] + "/")
         return redirect("/")
 
 
