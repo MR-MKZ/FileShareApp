644f5afda96a878c089c6816a9f048114eeaa279
Mr.MKZ <karimierfan74@gmail.com>
2025-01-06 20:07:23 +0330

    feat(links): copy all download links for files added to frontend.

--- static/js/main.js
+++ static/js/main.js
@@ -52,6 +52,47 @@ const isVideo = (extension) => {
   return videoExtensions.indexOf(extension) > -1;
 }
 
+/**
+ * @returns {Array} array of links
+ */
+const getAllLinks = () => {
+    const files = document.getElementsByClassName("fileItem");
+    let links = [];
+
+    for (let a of files) {
+        links.push(a.href)
+    }
+
+    return links;
+}
+
+const copyAllLinks = () => {
+    let links = getAllLinks();    
+
+    links = links.join("\n");   
+    
+    const copyText = async () => {
+        try {
+            await navigator.clipboard.writeText(links);
+        } catch (err) {
+            fallbackCopy(links);
+        }
+    };
+
+    const fallbackCopy = (links) => {
+        const textArea = document.createElement("textarea");
+        textArea.value = links;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand("copy");
+        document.body.removeChild(textArea);
+    };
+
+    copyText();
+
+    showNotification("Links copied successfully.");
+}
+
 const showNotification = (message) => {
     $(".notifContainer p").text(message)
     $(".notifContainer").addClass("show")
